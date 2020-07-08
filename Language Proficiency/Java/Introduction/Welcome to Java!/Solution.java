b5e3eac6c416d5072bb9d7d543bae36423eab491
Karel Vanhelden <karellism@users.noreply.github.com>
2020-07-08 04:17:18 +0200

    Create Solution.java

---
+++ Language Proficiency/Java/Introduction/Welcome to Java!/Solution.java
@@ -0,0 +1,9 @@
+// My Solution
+// Karel Vanhelden - karellism.com
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+        }
+}
