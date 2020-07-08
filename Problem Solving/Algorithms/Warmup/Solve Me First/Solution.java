f51985596168b72c09a16ae85e3171fd952d19cc
Karel Vanhelden <karellism@users.noreply.github.com>
2020-07-08 04:37:05 +0200

    Update Solution.java

--- Problem Solving/Algorithms/Warmup/Solve Me First/Solution.java
+++ Problem Solving/Algorithms/Warmup/Solve Me First/Solution.java
@@ -10,17 +10,17 @@ import java.util.regex.*;
 
 public class Solution {
     static int solveMeFirst(int a, int b) {
-        return a+b;
-   }
+        return a + b;
+    }
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a;
-        a = in.nextInt();
+        a = in .nextInt();
         int b;
-        b = in.nextInt();
+        b = in .nextInt();
         int sum;
         sum = solveMeFirst(a, b);
         System.out.println(sum);
-   }
+    }
 }
